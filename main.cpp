160ae694a39de65b5afe9377039c52d793138019
Tatiana Malinaricova <tatiana.malinaricova@gmail.com>
2018-09-30 21:13:39 +0200

    Zadanie 1.pocitanie znakov,slov a riadkov v zadanom texte

--- main.cpp
+++ main.cpp
@@ -20,23 +20,23 @@ int main(int argc, char *argv[]) //pocet parametrov, pole parametrov
 		if (std::string(argv[1]) == ("-c"))
 		{
 			//pocitanie znakov v texte
-			std::cout << "Zadajte retazec" << std::endl;
+			std::cout << "Pocitanie znakov v texte. Zadajte retazec" << std::endl;
 			getline(std::cin,retazec);
 			std::cout<<"Pocet znakov v retazci je: " << retazec.size() << std::endl;
 
-		//	std::istream s(std::string(argv[1]));
-		//	std::istream_iterator<char> it = ;
+		//	std::iftream f(argv[1]);
+		//	std::istream_iterator<char> my_it;
 		//	std::distance(std::istream_iterator<char>(s));
 		}
 		if (std::string(argv[1]) == ("-w"))
 		{
 			//pocitanie slov v texte
 			int poc=0;
-			std::cout << "Zadajte retazec" << std::endl;
+			std::cout << "Pocitanie slov v zadanom texte. Zadajte retazec" << std::endl;
 			getline(std::cin, retazec);
 
 			char nasled;
-			for (int i = 0; i<int(retazec.length()); i++)
+			for (int i = 0; i <	int(retazec.length()); i++)
 			{
 				nasled = retazec.at(i); 
 				if (isspace(retazec[i]))
@@ -45,14 +45,25 @@ int main(int argc, char *argv[]) //pocet parametrov, pole parametrov
 			std::cout << "\nPocet slov je: " << poc<< std::endl;
 			
 		}
-		if (argv[1] == ("-l"))
+		if (std::string(argv[1]) == ("-l"))
 		{
 			//pocitanie riadkov v texte
+			std::cout << "Pocitanie riadkov v zadanom texte. Zadajte retazec" << std::endl;
+			char c;
+			int poc = 0;
+
+			while (std::cin.get(c)) 
+			{ 
+				if (c == '\n')
+					poc++;
+
+				if (c == '\t')
+					break;
+			}
+			std::cout << "Pocet riadkov je: " << poc<<std::endl;
+
 
 		}
 	}
-
-
-	system("pause");
 	return 0;
 }
