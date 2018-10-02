82d573c062962504dc8ec8d669258dac207c8664
Tatiana Malinaricova <tatiana.malinaricova@gmail.com>
2018-10-02 12:47:58 +0200

    pridany unit test

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+#define CATCH_CONFIG_MAIN 
+#include "C:/Users/Tatia/Desktop/MPM/4.roèník/ZS/Tvorba softvérových systémov/zadanie1/ipch/UNITTEST1-3a0af776/include/catch.hpp"
+#include "C:/Users/Tatia/Desktop/MPM/4.roèník/ZS/Tvorba softvérových systémov/zadanie1/counter.h"
+
+TEST_CASE("Popis", "Skrateny nazov")
+{
+	//REQUIRE(bool );
+//apson 6x nech sa vola s tymi parametrami + dorobit nacitavanie zo suboru.
+}
