a80700846d21bd229364992cacd508ffd0328e94
Tatiana Malinaricova <tatiana.malinaricova@gmail.com>
2018-10-23 11:39:09 +0200

    application onPaint

---
+++ Application/ApplicationDlg.cpp
@@ -0,0 +1,282 @@
+
+// ApplicationDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Application.h"
+#include "ApplicationDlg.h"
+#include "afxdialogex.h"
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <stdio.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#ifndef MIN_SIZE
+#define MIN_SIZE 300
+#endif
+
+void CStaticImage::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
+{
+	GetParent()->SendMessage( CApplicationDlg::WM_DRAW_IMAGE, (WPARAM)lpDrawItemStruct);
+}
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg() : CDialogEx(IDD_ABOUTBOX) {}
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	void DoDataExchange(CDataExchange* pDX) override    // DDX/DDV support
+	{
+		CDialogEx::DoDataExchange(pDX);
+	}
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+// CApplicationDlg dialog
+
+CApplicationDlg::CApplicationDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_APPLICATION_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	image = new CImage();
+}
+
+void CApplicationDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_IMAGE, m_ctrlImage);
+}
+
+BEGIN_MESSAGE_MAP(CApplicationDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_COMMAND(ID_FILE_OPEN, OnFileOpen)
+	ON_UPDATE_COMMAND_UI(ID_FILE_OPEN, OnUpdateFileOpen)
+	ON_COMMAND(ID_FILE_CLOSE, OnFileClose)
+	ON_UPDATE_COMMAND_UI(ID_FILE_CLOSE, OnUpdateFileClose)
+	ON_WM_CLOSE()
+	ON_WM_SIZE()
+	ON_WM_SIZING()
+	ON_MESSAGE(WM_DRAW_IMAGE, OnDrawImage)
+	ON_WM_DESTROY()
+	ON_STN_CLICKED(IDC_IMAGE, &CApplicationDlg::OnStnClickedImage)
+END_MESSAGE_MAP()
+
+
+void CApplicationDlg::OnDestroy()
+{
+	Default();
+}
+
+LRESULT CApplicationDlg::OnDrawImage(WPARAM wParam, LPARAM lParam)
+{
+	LPDRAWITEMSTRUCT lpDI = (LPDRAWITEMSTRUCT)wParam;
+
+	CDC * pDC = CDC::FromHandle(lpDI->hDC);
+	::MessageBox(NULL, __T("Chyba pri zobrazeni file dialogu."), __T("Error"), MB_OK);
+
+	//DRAW BITMAP
+
+	return S_OK;
+}
+
+void CApplicationDlg::OnClose()
+{
+	EndDialog(0);
+}
+
+BOOL CApplicationDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+	CRect rct;
+
+	CRect rctClient;
+	GetClientRect(&rctClient);
+	
+	m_ctrlImage.GetWindowRect(&rct);
+	m_ptImage.x = rctClient.Width() - rct.Width();
+	m_ptImage.y = rctClient.Height() - rct.Height();
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CApplicationDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CApplicationDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+
+		int img_x = image->GetWidth();
+		int img_y = image->GetHeight();
+
+		/*	//skalovanie obrazku podla dialogoveho okna
+		float factor = 1;
+		if ((img_x <= dlg_x) && (img_y <= dlg_y)) {
+		if (dlg_x > dlg_y) {
+		factor = (float)dlg_y / (float)img_y;
+		}
+		else {
+		factor = (float)dlg_x / (float)img_x;
+		}
+		}
+		if ((img_x <= dlg_x) && (img_y > dlg_y)) {
+		factor = (float)dlg_y / (float)img_y;
+		}
+		if ((img_x > dlg_x) && (img_y <= dlg_y)) {
+		factor = (float)dlg_x / (float)img_x;
+		}
+		if ((img_x > dlg_x) && (img_y > dlg_y)) {
+		if (img_x > img_y) {
+		factor = (float)dlg_y / (float)img_y;
+		}
+		else {
+		factor = (float)dlg_x / (float)img_x;
+		}
+		}
+
+		*/
+
+		//	CDC *screenDC = GetDC();
+		CDC *pDC = GetDC();
+		CDC mDC;
+		mDC.CreateCompatibleDC(pDC);
+		CBitmap bitmap;
+		bitmap.CreateCompatibleBitmap(pDC, img_x, img_y);
+
+		CBitmap *p_bitmap = mDC.SelectObject(&bitmap);
+		mDC.SetStretchBltMode(HALFTONE);
+		image->StretchBlt(mDC.m_hDC, 0, 0, img_x, img_y, 0, 0, img_x, img_y, SRCCOPY);
+		mDC.SelectObject(p_bitmap);
+
+		m_ctrlImage.SetBitmap((HBITMAP)bitmap.Detach());
+		ReleaseDC(pDC);
+		
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CApplicationDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CApplicationDlg::OnFileOpen()
+{
+	// vytvorime file dialog na iba citanie a prepisom promptu, s filtrom .jpg a .png
+	CFileDialog dlg(TRUE, NULL, NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, _T("Jpg Files (*.jpg)|*.jpg|Png Files (*.png)|*.png||"));
+	
+	// zobrazi file dialog
+	if (dlg.DoModal() == IDOK) {
+		path_name = dlg.GetPathName();
+		OnPaint();
+		image->Load(path_name);
+
+	}
+	else {
+		::MessageBox(NULL, __T("Chyba pri zobrazeni file dialogu."), __T("Error"), MB_OK);
+	}
+}
+
+void CApplicationDlg::OnUpdateFileOpen(CCmdUI *pCmdUI)
+{
+	pCmdUI->Enable(TRUE);
+}
+
+
+void CApplicationDlg::OnFileClose()
+{
+
+}
+
+
+void CApplicationDlg::OnUpdateFileClose(CCmdUI *pCmdUI)
+{
+	pCmdUI->Enable(TRUE);
+}
+
+void CApplicationDlg::OnStnClickedImage()
+{
+	// TODO: Add your control notification handler code here
+}
